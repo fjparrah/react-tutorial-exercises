145aa98d58d2594f6cda7211a3805b000d95d624
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-09 02:12:18 +0000

    terminado

--- .learn/resets/01.3-building-a-layout/app.jsx
+++ .learn/resets/01.3-building-a-layout/app.jsx
@@ -14,16 +14,7 @@ const data = {
 
 // Modify the 'content' variable below to display the desired bootstrap card
 let content = (
-	<div className="card m-5">
-		<img alt="Card image cap" src={data.image} className="card-img-top" />
-		<div className="card-body">
-			<h5 className="card-title">{data.cardTitle}</h5>
-			<p className="card-text">{data.cardDescription}</p>
-			<a href={data.button.url} className="btn btn-primary">
-				{data.button.label}
-			</a>
-		</div>
-	</div>
+		<img src={data.image} />
 );
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
